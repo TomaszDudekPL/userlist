9f2cc252126ffb15950203e46001685d3312c488
TomaszDudekPL <tdkontakt@gamil.com>
2018-11-01 00:09:23 +0100

    user list app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+let allUsers = ['Karina', 'Tomasz', 'Bartek', 'Ola', 'Aleksandra', 'Janusz'];
+
+const UsersList = ({users})=> {
+
+	if(users.length > 0) {
+		return (<ul>
+			{users.map(user => <li key={user}>{user}</li>)}
+		</ul>);
+	}
+
+	return (
+		<p>NO RESULTS!</p>
+	)
+
+};
+
+class App extends React.Component {
+
+	constructor(){
+		super();
+		this.state = {
+			filteredArray: allUsers
+		}
+	}
+
+	filterUsers(text) {
+		return new Promise(resolve => {
+			const time = (Math.random() + 1) * 250;
+			setTimeout(() => {
+				const filteredUsers = allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()));
+				resolve(filteredUsers);
+			}, time) ;
+		});
+	}
+
+	takeAndFilterValue(e){
+		let text = e.target.value;
+		this.filterUsers(text).then(filteredUsers=>	this.setState({
+			filteredArray: filteredUsers
+		}));
+		if(text === null) {
+			this.setState({
+				filteredArray: allUsers
+			})
+		}
+	}
+
+	takeValue= () => {
+		let input = document.querySelector('#val');
+		console.log('value: ', input.value);
+		input.value = ''
+	};
+
+
+
+	render() {
+		return (<div>
+			<div className="ui input">
+				<input type="search" title="coś tam" id="val" />
+				<button onClick={this.takeValue}>Take Value</button>
+				<input type="search" onInput={this.takeAndFilterValue.bind(this)}/>
+			</div>
+			<UsersList users={this.state.filteredArray}/>
+		</div>);
+	}
 }
 
 export default App;
