7a603a7b837bff49bc975a75fa50cb11c1e29dbe
TomaszDudekPL <tdkontakt@gamil.com>
2018-11-01 12:48:00 +0100

    Updates

---
+++ precache-manifest.4205e94146b5afd0ecea9dd5e3b4ce90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6ca465b2a3f7653ee34",
+    "url": "/userlist/static/js/runtime~main.f6ca465b.js"
+  },
+  {
+    "revision": "95af3f6961f078bb0848",
+    "url": "/userlist/static/js/main.95af3f69.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/userlist/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "95af3f6961f078bb0848",
+    "url": "/userlist/static/css/main.d20c8fb8.chunk.css"
+  },
+  {
+    "revision": "f8931c9b19772cdaf8fc335a64bf474c",
+    "url": "/userlist/index.html"
+  }
+];
